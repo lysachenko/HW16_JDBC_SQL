3ae0ed34d7ae34f12e88b7f106f0a1a7e5c1af7f
Roman Lysachenko <lysachenko.roman.1099@gmail.com>
2020-08-12 14:30:38 +0300

    add MySQL DB

--- src/main/java/com/lysachenko/util/JDBCUtil.java
+++ src/main/java/com/lysachenko/util/JDBCUtil.java
@@ -9,10 +9,15 @@ public class JDBCUtil {
     public static String H2_USERNAME = "sa";
     public static String H2_PASSWORD = "";
 
+    public static String MY_SQL_URL = "jdbc:mysql://localhost/students_db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    public static String MY_SQL_USERNAME = "root";
+    public static String MY_SQL_PASSWORD = "root";
+
     public static Connection getConnection() {
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(H2_URL, H2_USERNAME, H2_PASSWORD);
+            //connection = DriverManager.getConnection(MY_SQL_URL, MY_SQL_USERNAME, MY_SQL_PASSWORD);
         } catch (SQLException e) {
             e.printStackTrace();
         }
